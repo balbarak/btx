7efa6f5c7db1be770fe5bb4ada1b1ff13713391b
balbarak <balbarak@live.com>
2018-02-10 00:45:43 +0300

    add test client

---
+++ src/Btx.Client.Test/ViewModels/MainViewModel.cs
@@ -0,0 +1,78 @@
+﻿using Btx.Client.Test.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Btx.Client.Test.ViewModels
+{
+    public class MainViewModel : BaseViewModel
+    {
+        public BtxClient Client { get; set; } = new BtxClient();
+
+        public ICommand ConnectCommand { get; set; }
+
+        public ICommand DisconnectCommand { get; set; }
+
+        private string log = "";
+        public string Log
+        {
+            get { return log; }
+            set { log = value; OnPropertyChanged(); }
+        }
+
+        private bool isConnected;
+        public bool IsConnected
+        {
+            get { return isConnected; }
+            set { isConnected = value; OnPropertyChanged(); }
+        }
+
+
+        public MainViewModel()
+        {
+            ConnectCommand = new DelegateCommand(async (data) => { await Connect(); });
+            DisconnectCommand = new DelegateCommand(async (data) => { await Disconnect(); });
+
+            Client.OnConnected += OnConnected;
+            Client.OnClosed += OnClosed;
+        }
+
+        private Task OnClosed(Exception ex)
+        {
+            AddLog($"Disconnected: {ex?.ToString()}");
+
+            IsConnected = false;
+
+            return Task.FromResult(0);
+
+        }
+
+        public Task OnConnected(object sender)
+        {
+            AddLog("Connected");
+            IsConnected = true;
+            return Task.FromResult(0);
+        }
+
+        public async Task Connect()
+        {
+            AddLog("Connecting ...");
+
+            if (!IsConnected)
+                await Client.Connect();
+        }
+
+        public async Task Disconnect()
+        {
+            await Client.Disconnect();
+        }
+
+        public void AddLog(string text)
+        {
+            Log += $"{text}\n";
+        }
+    }
+}
---
+++ src/Btx.Client/BtxClient.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Btx.Client
+{
+    
+    public class BtxClient
+    {
+        private string _connectionUrl = "http://localhost:5000/btx";
+
+        public delegate Task BtxConnectionEventHandler(object sender);
+        public delegate Task BtxConnectionCloseEventHanlder(Exception ex);
+
+        public event BtxConnectionEventHandler OnConnected;
+        public event BtxConnectionCloseEventHanlder OnClosed;
+        
+        private HubConnection Connection { get; set; }
+
+        public BtxClient()
+        {
+            Connection = new HubConnectionBuilder()
+            .WithUrl(_connectionUrl)
+            .WithConsoleLogger()
+            .Build();
+
+            SetupEvents();
+        }
+
+        public async Task Connect()
+        {
+            await Connection.StartAsync();
+        }
+
+        public async Task Disconnect()
+        {
+            await Connection.DisposeAsync();
+        }
+
+        private void SetupEvents()
+        {
+            Connection.Connected += OnConnectedInternal;
+            Connection.Closed += OnClosedInternal;
+        }
+
+        private Task OnClosedInternal(Exception arg)
+        {
+            OnClosed?.Invoke(arg);
+
+            return Task.FromResult(0);
+        }
+
+        private async Task OnConnectedInternal()
+        {
+            await OnConnected?.Invoke(this);
+        }
+        
+    }
+}
