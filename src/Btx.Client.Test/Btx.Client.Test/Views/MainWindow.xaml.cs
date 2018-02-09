b4db88b46f41fcd929ff9654a3d00dced01fc785
Bader Albarrak <balbarak@live.com>
2018-02-09 21:06:21 +0300

    add test client

---
+++ src/Btx.Client.Test/Btx.Client.Test/Views/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Btx.Client.Test.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var connection = new HubConnectionBuilder()
+                .WithUrl("http://localhost:5000/btx")
+                .WithConsoleLogger()
+                .Build();
+            
+            
+
+            await connection.StartAsync();
+        }
+    }
+}
