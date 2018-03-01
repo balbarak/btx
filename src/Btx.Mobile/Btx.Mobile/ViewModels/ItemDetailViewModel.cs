5352ffb32bb336a259c512d4417eff603ed09b21
Bader Albarrak <balbarak@live.com>
2018-03-01 17:07:18 +0300

    add mobile project

---
+++ src/Btx.Mobile/Btx.Mobile/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Btx.Mobile.Models;
+
+namespace Btx.Mobile.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
