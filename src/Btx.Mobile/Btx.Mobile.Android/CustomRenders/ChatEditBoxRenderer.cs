a3e6513154da6983f875e314c2f9fa27c10d613c
Bader Albarrak <balbarak@live.com>
2018-03-03 15:40:17 +0300

    run on iphone

--- src/Btx.Mobile/Btx.Mobile.Android/CustomRenders/ChatEditBoxRenderer.cs
+++ src/Btx.Mobile/Btx.Mobile.Android/CustomRenders/ChatEditBoxRenderer.cs
@@ -31,19 +31,7 @@ namespace Btx.Mobile.Droid.CustomRenders
 
             if (Control != null)
             {
-
-                //Control.LayoutParameters = new ViewGroup.LayoutParams(0, ViewGroup.LayoutParams.WrapContent);
-
-                //Control.VerticalFadingEdgeEnabled = true;
-                //Control.VerticalScrollBarEnabled = false;
-
                 Control.SetBackgroundColor(Color.Transparent.ToAndroid());
-                //Control.SetPadding(40, 30, 30, 30);
-
-                //Control.SetEms(13);
-                //Control.SetMaxLines(5);
-                //Control.SetSingleLine(false);
-
                 Control.Hint = "Send a message ...";
                 
             }
---
+++ src/Btx.Mobile/Btx.Mobile.iOS/CustomRenders/ChatEditBoxRenderer.cs
@@ -0,0 +1,27 @@
+﻿//using Btx.Mobile.iOS.CustomRenders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+//[assembly: ExportRenderer(typeof(ChatEditBox), typeof(ChatEditBoxRenderer))]
+//namespace Btx.Mobile.iOS.CustomRenders
+//{
+//    public class ChatEditBoxRenderer : EditorRenderer
+//    {
+
+//        protected override void OnElementChanged(ElementChangedEventArgs<Editor> e)
+//        {
+//            base.OnElementChanged(e);
+
+//            if (Control != null)
+//            {
+//                //Control.SetBackgroundColor(Color.Transparent.ToAndroid());
+//                //Control.Hint = "Send a message ...";
+
+//            }
+
+//        }
+//    }
+//}
