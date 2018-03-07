0d0d7c9767a2766dbb332f0360e0e3e1fd838fac
balbarak <balbarak@live.com>
2018-03-07 04:37:30 +0300

    update

---
+++ src/Btx.Mobile/Btx.Mobile/Helpers/ImageResourceExtension.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Btx.Mobile.Helpers
+{
+    public class ImageResourceExtension : IMarkupExtension
+    {
+        public string Image { get; set; }
+
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (Image == null)
+            {
+                return null;
+            }
+            // Do your translation lookup here, using whatever method you require
+            var imageSource = ImageSource.FromResource($"Btx.Mobile.Images.{Image}");
+
+            return imageSource;
+        }
+    }
+
+
+}
