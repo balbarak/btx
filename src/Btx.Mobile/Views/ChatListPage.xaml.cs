b66c2f30c161aad8a3cb69a413fbf6803dc3e917
Bader Albarrak <balbarak@live.com>
2018-05-13 00:16:14 +0300

    update

--- src/Btx.Mobile/Views/ChatListPage.xaml.cs
+++ src/Btx.Mobile/Views/ChatListPage.xaml.cs
@@ -30,18 +30,7 @@ namespace Btx.Mobile.Views
         {
             base.OnAppearing();
         }
-
-        private void OnSelected(object sender, SelectedItemChangedEventArgs e)
-        {
-            if (e.SelectedItem == null)
-            {
-                return; //ItemSelected is called on deselection, which results in SelectedItem being set to null
-            }
-
-            ((ListView)sender).SelectedItem = null; //uncomment line if you want to disable the visual selection state.
-            
-        }
-
+        
         private void OnTabbed(object sender, ItemTappedEventArgs e)
         {
             if (ViewModel.IsBusy)
